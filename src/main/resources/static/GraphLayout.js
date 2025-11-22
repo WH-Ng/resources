a8628759ae6de0d8069425bb33afe2d456949951
Wayne Hann Ng <wayne@MacBook-Pro-6.local>
2025-11-22 16:37:11 +1100

    fixed the double repo

--- src/main/resources/static/GraphLayout.js
+++
@@ -1,9 +0,0 @@
-class GraphLayout extends allure.components.AppLayout {
-  initialize(config) {
-    this.config = config;
-  }
-
-  getContentView() {
-    return new GraphGridView({ config: this.config });
-  }
-}
